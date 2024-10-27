ea87e339f89fc78b1e2efb22d138e7eef192f635
Mausham Neupane <neupanemausham2060@gmail.com>
2024-10-27 20:08:33 +0545

    loading screen added

---
+++ Assets/LoadingScreen.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingScreen : MonoBehaviour
+{
+    public GameObject loadingPanel;
+
+    void Start()
+    {
+        loadingPanel.SetActive(true);
+        StartCoroutine(LoadAsyncScene());
+    }
+
+    IEnumerator LoadAsyncScene()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(1);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        loadingPanel.SetActive(false);
+    }
+}
