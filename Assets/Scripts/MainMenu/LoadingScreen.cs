83d65f64230eb11e5dbda347cd1e1b784d493f7c
Mausham <neupanemausham2060@gmail.com>
2024-12-02 21:29:30 +0545

    levels player prefs code removed (BUG) v1.1.1

--- Assets/Scripts/MainMenu/LoadingScreen.cs
+++ Assets/Scripts/MainMenu/LoadingScreen.cs
@@ -12,10 +12,6 @@ public class LoadingScreen : MonoBehaviour
         
         loadingPanel.SetActive(true);
         StartCoroutine(LoadAsyncScene());
-        PlayerPrefs.DeleteAll();
-
-        // Ensure the changes are saved immediately
-        PlayerPrefs.Save();
     }
 
     IEnumerator LoadAsyncScene()
